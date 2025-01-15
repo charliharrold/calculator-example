c810d83850859f1c8969a6036d455784784d3e37
charliharrold <charliharrold@gmail.com>
2025-01-14 22:41:44 -0800

    Add styling to calculator

--- client/src/App.js
+++ client/src/App.js
@@ -28,8 +28,8 @@ function App() {
 
   return (
     <div>
-      <div>
-        {display}
+      <div className="rounded-box">
+        <p className="display">{display}</p>
       </div>
       <div className="grid-container">
         <button onClick={() => handleButtonClick('(')} className="grid-button">(</button>
